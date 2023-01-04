5f0e9c2d93d932a61b737ce946eb0e4d5295f635
Sergey Baryshnikov <sergey.baryshnikov89@gmail.com>
2023-01-04 18:19:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+let numA = 12;
+let numB = -4;
+let numC = 999;
+let result;
+
+// Є дві змінні, якщо вони з однаковим знаком - то перемножити і вивести результат,
+// якщо з різними знаками - то поділити і вивести результат,
+// якщо одне зі значень 0 - то покласти в результат значення іншої змінної і вивести результат,
+// якщо обидва нулі - то покласти в результат NaN і вивести результат.
+
+if ((numA === 0 && numB !== 0) || (numA !== 0 && numB === 0)) {
+  result = numC;
+} else if ((numA > 0 && numB > 0) || (numA < 0 && numB < 0)) {
+  result = numA * numB;
+} else if ((numA > 0 && numB < 0) || (numA < 0 && numB > 0)) {
+  result = numA / numB;
+} else {
+  result = NaN;
+}
+console.log(result);
+
+if (numA * numB === 0 && numA !== numB) {
+  result = numC;
+} else if (numA * numB > 0) {
+  result = numA * numB;
+} else if (numA * numB < 0) {
+  result = numA / numB;
+} else {
+  result = NaN;
+}
+console.log(result);
+
+if (numA * numA === 0) {
+  if (numA !== numB) {
+    result = numC;
+  } else {
+    result = NaN;
+  }
+} else if (numA * numB > 0) {
+  result = numA * numB;
+} else if (numA * numB < 0) {
+  result = numA / numB;
+}
+console.log(result);
+
+// Якщо число додатнє, парне, кратне 3 і 6 одночасно - вивести win, інакше - lose.
+// Першим обробляти варіант з lose.
+
+if (numA < 6 || numA % 6) {
+  console.log("lose");
+} else {
+  console.log("win");
+}
